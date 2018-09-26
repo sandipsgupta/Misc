2b8a7fbf2181c29db962236f006b796ddbe38f98
sandip sengupta <sandipsgupta@gmail.com>
2018-09-26 01:01:44 -0500

    Move

---
+++ src/operator_test.java
@@ -0,0 +1,20 @@
+public class operator_test {
+    public static void main(String[] args) {
+       // int s = 0;
+        String s = "0";
+        // int y = 0;
+        /*for(int x = 1; x<10; x++){
+            s = s+x;
+        }*/
+        //while (y<10) {
+        for (int x = 1; x < 10; x++) {
+            s = s + x;
+            // x=y;
+
+            //s=s+y;*/
+            System.out.println("The value of s is\t" + s);
+
+        }
+
+    }
+}
