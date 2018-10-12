67de01cae643923425f04a1212a5e04a6e125c1b
sandip sengupta <sandipsgupta@gmail.com>
2018-10-12 01:10:29 -0500

    New Array test

--- src/Hobbits.java
+++ src/Hobbits.java
@@ -1,19 +1,20 @@
-class Hobbits {
+public class Hobbits {
     String name;
     public static void main(String [] args) {
         Hobbits [] h = new Hobbits[3];
+        h[1] = new Hobbits();
+        h[2] = new Hobbits();
+        h[0] = new Hobbits();
         int z = 0;
         while (z < 4) {
             z = z + 1;
-            h[z] = new Hobbits();
             h[z].name = "bilbo";
-            if (z == 1) {
-                h[z].name = "Frodo"
-            }
+            if (z == 1)
+                h[z].name = "Frodo";
             if (z == 2) {
                 h[z].name = "am";
             }
-            System.out.print(h[z].name + "is a");
+            System.out.print(h[z].name + "\tis a");
             System.out.println("good Hobbit name");
         }
     }
